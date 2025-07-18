b52b113b2c0c701704b96035dc3e7bcc55c3cbbe
nicolasnicholson <nicolasnicholson191@gmail.com>
2025-07-17 22:41:07 -0300

    spotify-clone

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 
 import vercel from "@astrojs/vercel/serverless";
+// astro.config.mjs
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), react()],
+  integrations: [tailwind(), svelte(), react(), vercel()],
   output: 'server',
   adapter: vercel()
 });
